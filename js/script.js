5b00012e3cb92c243ca93cc925e51496a17e917c
iCubeVK <199650705+ICubeVK@users.noreply.github.com>
2026-09-14 22:38:30 +0200

    Final Commit

--- js/script.js
+++ js/script.js
@@ -1,29 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const testoBase =
-        "Come in molti testi medievali, la numerologia biblica gioca un ruolo strutturale fondamentale, " +
-        "ciascuno dei 4 elementi è accompagnato da 4 indicatori, questo è collegato al ";
-
-    const elementi = ["vento", "acqua", "fuoco", "terra"];
-
-    elementi.forEach(elemento => {
-        document.querySelectorAll("span." + elemento).forEach(span => {
-            span.setAttribute("title", testoBase + elemento);
-        });
-    });
-});
-
 // Radios  
 const radios_containers = document.querySelectorAll('input[name="n_slide_container"]')
 const display = document.getElementById('left_container')
-const containers_array = Array.from(document.getElementsByClassName("slide_container"))
+const containers_array = Array.from(document.getElementsByClassName("slide_group"))
 
 // Add event listeners to each radio button
 radios_containers.forEach(radio => {
-    radio.addEventListener('change', handleChangeContainer)
+    radio.addEventListener('change', handleButtonChangeContainer)
 })
 
 // Call the function on page load to set initial state
-handleChangeContainer.call(document.querySelector('input[name="n_slide_container"]:checked'))
+handleButtonChangeContainer.call(document.querySelector('input[name="n_slide_container"]:checked'))
 
 
 
@@ -50,12 +36,12 @@ function createSlideRadioGroup(container) {
     group.setAttribute("role", "radiogroup")
 
     slides.forEach((slide, index) => {
-        const id = `slide_radio_${container_type}_${index}`
+        const id = `slide_radio_${container_type}_${index+1}`
 
         // Radio
         const input = document.createElement("input")
         input.type = "radio"
-        input.className = "btn-check"
+        input.className = "btn-check slide_btn"
         input.name = "slide_selector"
         input.id = id
         input.autocomplete = "off"
@@ -81,8 +67,11 @@ function createSlideRadioGroup(container) {
 
 
 // Function to handle change slide container event
-function handleChangeContainer() {
-    const selected_value = this.value
+function handleButtonChangeContainer() {
+    handleChangeContainer(this.value)
+}
+
+function handleChangeContainer(selected_value) {
 
     // Clean selected
     containers_array.forEach(container => {
@@ -97,7 +86,7 @@ function handleChangeContainer() {
     })
 
     // Fetch selected container
-    selected_container = document.getElementsByClassName("slide_container " + selected_value)[0]
+    selected_container = document.getElementsByClassName("slide_group slide_group" + selected_value)[0]
     var container_type = selected_container.classList[1]
 
     const slides = Array.from(selected_container.getElementsByClassName(container_type))
@@ -110,3 +99,72 @@ function handleChangeContainer() {
     // Create buttons dynamically
     createSlideRadioGroup(selected_container)
 }
+
+function updateButtons(container_value, slide_value) {
+    Array.from(radios_containers)
+        .forEach(s => s.checked = false)
+    Array.from(document.getElementsByClassName("slide_btn"))
+        .forEach(s => s.checked = false)
+    document.getElementById(`n_slide_container_radio${container_value}`).checked = true
+    document.getElementById(`slide_radio_slide_group${container_value}_${slide_value}`).checked = true
+}
+
+// Button support
+document.addEventListener("keydown", event => {
+    if (event.key.startsWith("Arrow")) {
+        event.preventDefault()
+
+        now_group = document.getElementsByClassName("slide_group selected")[0]
+        now_slide = now_group.getElementsByClassName("slide selected")[0]
+
+        now_group_value = parseInt(Array.from(now_group.classList).filter(
+            n => n.startsWith("slide_group")
+        ).at(1).slice(11))
+        now_slide_value = parseInt(Array.from(now_slide.classList).filter(
+            n => n.startsWith("slide") && !n.startsWith("slide_group")
+        ).at(1).slice(5))
+
+        n_groups = document.getElementsByClassName("slide_group").length
+        n_slides = now_group.getElementsByClassName("slide").length
+        switch(event.key) {
+            case "ArrowUp":
+                if (now_slide_value === 1) 
+                    next_slide_value = n_slides
+                else 
+                    next_slide_value = now_slide_value - 1
+
+                next_slide = now_group.querySelectorAll(`.slide.slide${next_slide_value}`)[0]
+                handleChangeSlide(now_group, next_slide)
+                updateButtons(now_group_value, next_slide_value)
+                break
+            case "ArrowDown":
+                if (now_slide_value === n_slides) 
+                    next_slide_value = 1
+                else 
+                    next_slide_value = now_slide_value + 1
+
+                next_slide = now_group.querySelectorAll(`.slide.slide${next_slide_value}`)[0]
+                handleChangeSlide(now_group, next_slide)
+                updateButtons(now_group_value, next_slide_value)
+                break
+            case "ArrowLeft":
+                if (now_group_value === 1) 
+                    next_group_value = n_groups
+                else 
+                    next_group_value = now_group_value - 1
+
+                handleChangeContainer(next_group_value)
+                updateButtons(next_group_value, 1)
+                break
+            case "ArrowRight":
+                if (now_group_value === n_groups) 
+                    next_group_value = 1
+                else 
+                    next_group_value = now_group_value + 1
+
+                handleChangeContainer(next_group_value)
+                updateButtons(next_group_value, 1)
+                break
+        }
+    }
+})
